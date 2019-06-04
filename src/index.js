797780988566aef065188125e9d72d8b3c290fd5
David Liu <david-lky@hotmail.com>
2019-06-04 16:30:39 -0400

    should not deploy

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Book from './Book';
-import Done from './Done';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
@@ -11,8 +9,6 @@ ReactDOM.render(
   <Router>
     <React.Fragment>
       <Route path="/" exact component={App} />
-      <Route path="/book/" component={Book} />
-      <Route path="/done/" component={Done} />
     </React.Fragment>
   </Router>,
   document.getElementById('root')
